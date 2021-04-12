adf5b63b34d03510ac8f78904ff2911bba1ab796
andy-aguilar <aaguil3@gmail.com>
2021-04-12 13:00:40 -0400

    Changes during lecture

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                What do you want to know about me?
+            </div>
+        )
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Root from "./Root"
+import SignIn from "./SignIn"
+import About from "./About"
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>HI</header>
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Root />
+          </Route>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
